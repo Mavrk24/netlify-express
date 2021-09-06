18edfb8534b56a84123484fbf1cb8a7504261ae2
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-06 22:23:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request, session
+from qTree import display, getQ, returnQ,valuate
+from anytree import Node, RenderTree,AsciiStyle,PreOrderIter
+from anytree.dotexport import RenderTreeGraph
+from operator import itemgetter
 
 app = Flask(__name__)
 
@@ -23,6 +27,68 @@ data = [
 @app.route('/')
 def hello():
     return "Hello Flask-Heroku"
+
+
+@app.route("/username", methods=['POST'])
+def login():
+    username = request.get_json()
+    # username = request.form.get("username", "") or request.form["username"]
+    session["username"]=username
+    return {'user': username} # from flask import jsonify 
+
+
+@app.route("/subentry" , methods=['POST'])
+def sub():
+    entry = request.get_json()
+    session["entry"]=entry
+    return entry 
+
+@app.route("/request")
+def getreq():
+    key = [1,[1,0,1,0,1],[1,0,1,0,1],[1,0,1,0,1],0] #sample_data
+    return {'r_key': key }
+
+@app.route("/subrequest", methods=['POST'])
+def getarr():
+    arr = request.get_json
+    return arr
+
+@app.route("/display")
+def tree():
+    return {'text': getQ()}
+
+def merge(list1, list2):
+      
+    merged_list = []
+    for i in range(max((len(list1), len(list2)))):
+  
+        while True:
+            try:
+                tup = (list1[i], list2[i])
+            except IndexError:
+                if len(list1) > len(list2):
+                    list2.append('')
+                    tup = (list1[i], list2[i])
+                elif len(list1) < len(list2):
+                    list1.append('')
+                    tup = (list1[i], list2[i])
+                continue
+  
+            merged_list.append(tup)
+            break
+    return merged_list
+
+@app.route("/intervention")
+def intervent():
+    num=valuate()
+    new_array = []
+    arr = session.get("entry")['payload']
+    index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    array = merge(arr,index)
+    array.sort(key=itemgetter(0),reverse=True)
+    array = merge(array,num)
+    new_array = sorted(array,key=itemgetter(1),reverse=True)
+    return {'text': new_array}
 
 
 @app.route('/api', methods=['GET'])
