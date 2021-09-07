db5ed5af3698bf3e230b435617b60a04d4ebb639
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-07 13:22:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const app = express();
 
 app.use(cors());
 
+
+app.get('/', (req, res) => res.send('Working!!!'));
+
+
 app.use('/login', (req, res) => {
   res.send({
     token: 'test123'
   });
 });
-
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
