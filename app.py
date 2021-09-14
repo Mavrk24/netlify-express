6f9097e9d97b0473b3241b4a8d8fac88953d6b86
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-15 00:03:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def intervent():
     num=valuate()
     new_array = []
     arr = request.get_json(force=True)['payload']
-    index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    index = [0,1,2,3,4,5,6,7,8,9,10,11]
     array = merge(arr,index)
     array.sort(key=itemgetter(0),reverse=True)
     array = merge(array,num)
