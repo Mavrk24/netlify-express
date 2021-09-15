681c2fb4152183337752cfc29d1fb37919375cd0
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-15 21:43:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -97,8 +97,7 @@ def intervent():
 @app.route("/mdp") #, methods=['POST'])
 def markov():
     #value = int(request.get_json(force=True)['int_value'])
-    addcost(60)
-    mdp = MDP(N=value)
+    mdp = MDP(N=60)
     payload = valueiteration(mdp)
     data = {'action': list(payload.items())[-1][-1]}
     return jsonify(data)
