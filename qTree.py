2424a333465a9ac774e2ab9d408d6e1f9a403c8a
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-22 22:06:21 +0700

    Update qTree.py

--- qTree.py
+++ qTree.py
@@ -1,20 +1,19 @@
 from anytree import Node, RenderTree,AsciiStyle,PreOrderIter
 from anytree.dotexport import RenderTreeGraph
-
 root = Node('root', value = 'null', label='') 
-a1 = Node('Do you use laptop for > 3 hours per day ?', parent=root, value='null',children=[ #1
-    Node('Do you use laptop on sofa / on bed ?',  value='null',label='Have a life'),#2
-    Node('Do you use laptop on lap ?',  value='null',label='Have a life'), #3
-    Node('Do you have to look down on screen ?', value='null',label='Have a life')], label='') #4s
-a2 = Node('Do you have to look while you type ?', parent=root , value='null',label='')
-b11 = Node('Do you use iPad/Tablet > 3 hours per day ?', parent=root, value='null',children=[
-    Node('Do you use iPad/Tablet on table / on lap (or bed) ?',  value='null',label='')], label='') #7
-b2 = Node('Do you use without standing cases ?', parent=root, value='null',label='') #8
-c = Node('Do you do manual work (drawing, drafting, sketching) ?', parent=root, value='null',label='')
-c1 = Node('Do you do the work on a table', parent=c, value='null',label='')
-c11 = Node('Is it a slope table ?', parent=c1, value='null',label='') #11s
-d = Node('Do you use a chair without arm rest ?', parent=root, value='null',label='')
-d1 = Node('Do you use a chair without lumbar back support ?', parent=d, value='null',label='')
+a1 = Node('คุณใช้งานคอมพิวเตอร์หรือโน๊ตบุ๊คมากกว่า 3 ชั่วโมงต่อวันหรือไม่ ?', parent=root, value='null',children=[ #1
+    Node('คุณใช้งานคอมพิวเตอร์หรือโน๊ตบุ๊คบนเตียงหรือโซฟาหรือไม่ ?',  value='null',label='Have a life'),#2
+    Node('คุณใช้งานคอมพิวเตอร์หรือโน๊ตบุ๊คบนหน้าตัก ?',  value='null',label='Have a life'), #3
+    Node('คุณต้องก้มลงเพื่อมองหน้าจอหรือไม่  ?', value='null',label='Have a life')], label='') #4s
+a2 = Node('คุณต้องก้มลงขณะพิมพ์หรือไม่ ?', parent=root , value='null',label='')
+b11 = Node('คุณใช้งาน iPad หรือ Tablet มากกว่า 3 ชั่วโมงต่อวันหรือไม่ ?', parent=root, value='null',children=[
+    Node('คุณใช้งาน iPad หรือ Tablet บนโต๊ะ, บนเตียงหรือบนหน้าตักหรือไม่ ?',  value='null',label='')], label='') #7
+b2 = Node('คุณใช้งานโดยไม่มีอุปกรณ์เสริมในการตั้ง เช่น Standing case หรือไม่ ?', parent=root, value='null',label='') #8
+c = Node('คุณทำงานวาดเขียนบ่อยหรือไม่ ?', parent=root, value='null',label='')
+c1 = Node('คุณทำงานวาดเขียนบนโต๊ะหรือไม่', parent=c, value='null',label='')
+c11 = Node('เก้าอี้ที่คุณนั่งไม่มีที่พักแขนใช่หรือไม่ ?', parent=c1, value='null',label='') #11s
+d = Node('เก้าอี้ที่คุณนั่งไม่มีที่พักแขนใช่หรือไม่ ?', parent=root, value='null',label='')
+d1 = Node('เก้าอี้ที่คุณนั่งไม่มีส่วนที่รอลรับนำ้หนักส่วนเอวใช่หรือไม่ ?', parent=d, value='null',label='')
 
 arr = []
 for a in PreOrderIter(root):
